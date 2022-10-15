56d4301f25dcc96cf7420e1e3eb0082ad3c74f54
Arrio Gonsalves <arriogonsalves03@gmail.com>
2022-10-14 19:30:15 -0500

    Update Rudder Control.ino

--- Rudder Control/Rudder Control.ino
+++ Rudder Control/Rudder Control.ino
@@ -1,14 +1,17 @@
 #include <Servo.h>
 
-byte servoPin = 9; // Signal pin for the servo.
-byte potentiometerPin = A0; // analog input pin for the potentiometer.
-Servo servo;
+const byte servoPin = 9; // Signal pin for the servo.
+const byte potentiometerPin = A0; // analog input pin for the potentiometer.
+Servo servo; // Declaring an instance of the servo class as an object
+int analogVal; // Declaring a int to store the analog values from the potentiometer
 
 void setup() {
-
+  servo.attach(servoPin); // Assigning the pin to the servo object to initiate control
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  analogVal = analogRead(potentiometerPin); // To read the values from the potentiometer
+  analogVal = map(analogVal, 0, 1023, 0, 180);  // Mapping the direction of the analog values (from 0 - 1023 to 0 - 180)
+  servo.write(analogVal); // Sends the mapped value to servo
+  delay(15); //Delays loop by 15ms
 }
