6ad1e270692272e723added3039b4abf027f77c7
Arrio Gonsalves <arriogonsalves03@gmail.com>
2022-10-14 19:19:58 -0500

    Create Rudder Control.ino

---
+++ Rudder Control/Rudder Control.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+byte servoPin = 9; // Signal pin for the servo.
+byte potentiometerPin = A0; // analog input pin for the potentiometer.
+Servo servo;
+
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
